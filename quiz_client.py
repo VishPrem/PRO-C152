e0e2da6e3f15a02cd9e1b11028326a0fa4990a28
VishPrem <91227044+VishPrem@users.noreply.github.com>
2024-07-31 18:53:41 -0400

    Add files via upload

---
+++ quiz_client.py
@@ -0,0 +1,31 @@
+import socket
+from threading import Thread
+
+nickname = input("Please enter your nickname: ")
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ip_address = "127.0.0.1"
+port = 8000
+client.connect((ip_address, port))
+
+def receive():
+    while True:
+        try:
+            message = client.recv(2048).decode("utf-8")
+            if message == "NICKNAME":
+                client.send(nickname.encode("utf-8"))
+            else:
+                print(message)
+        except:
+            print("An error has occured.")
+            client.close()
+
+def write():
+    while True:
+        message = input()
+        client.send(message.encode("utf-8"))
+
+recieve_thread = Thread(target=receive)
+recieve_thread.start()
+
+write_thread = Thread(target=write)
+write_thread.start()
